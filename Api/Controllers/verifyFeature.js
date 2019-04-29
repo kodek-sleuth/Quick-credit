8719c25414f78cb78020fb547b1875b1a7ddf394
Mugerwa Joseph <josekodek2000@gmail.com>
2019-04-29 19:40:37 +0300

    Creating verification routes

--- Api/Controllers/verifyFeature.js
+++ Api/Controllers/verifyFeature.js
@@ -1,3 +1,4 @@
+/* eslint-disable brace-style */
 /* eslint-disable no-trailing-spaces */
 /* eslint-disable comma-dangle */
 /* eslint-disable indent */
@@ -14,16 +15,79 @@ const connectionString = process.env.QUICK_CREDIT_DB;
 const pool = new Pool({ connectionString: connectionString });
 
 exports.verifyUser = (req, res, next) => {
-    const Email = req.param.Email;
+    const Email = req.params.Email;
 
     const verifyUserQuery = `Update users SET status='Verified' where email='${Email}'`;
 
     pool.query(verifyUserQuery)
     .then((result) => {
-        res.status(200).json({
-            Status: 200,
-            Data: result,
-            Success: 'Successfully Verified User'
-        }); 
+        pool.query(`Select * from users where email='${Email}'`)
+        .then((data) => {
+            if (data.rowCount > 0)
+            {
+                const dataFound = data.rows;
+                res.status(200).json({
+                    Status: 200,
+                    Data: {
+                        Fullname: dataFound[0].fullname,
+                        Email: dataFound[0].email,
+                        Status: dataFound[0].status,
+                        Address: dataFound[0].address
+                    },
+                    Success: 'Successfully Verified User'
+                }); 
+            }
+        })
+        .catch((error) => {
+            res.status(500).json({
+                Status: '500',
+                Error: 'Failed to Fetch Results'
+            });
+        });
+    })
+    .catch((error) => {
+        res.status(500).json({
+            Status: '500',
+            Error: 'Failed to Fetch Results'
+        });
+    });
+};
+
+exports.rejectUser = (req, res, next) => {
+    const Email = req.params.Email;
+
+    const rejectUserQuery = `Update users SET status='Rejected' where email='${Email}'`;
+
+    pool.query(rejectUserQuery)
+    .then((result) => {
+        pool.query(`Select * from users where email='${Email}'`)
+        .then((data) => {
+            if (data.rowCount > 0)
+            {
+                const dataFound = data.rows;
+                res.status(200).json({
+                    Status: 200,
+                    Data: {
+                        Fullname: dataFound[0].fullname,
+                        Email: dataFound[0].email,
+                        Status: dataFound[0].status,
+                        Address: dataFound[0].address
+                    },
+                    Success: 'Successfully Rejected User'
+                }); 
+            }
+        })
+        .catch((error) => {
+            res.status(500).json({
+                Status: '500',
+                Error: 'Failed to Update and Fetch'
+            });
+        });
+    })
+    .catch((error) => {
+        res.status(500).json({
+            Status: '500',
+            Error: 'Failed to Update and Fetch'
+        });
     });
 };
