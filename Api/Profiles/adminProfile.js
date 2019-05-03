fd7e9f97897d841b6a0ca57dcda86a85910e2c53
Mugerwa Joseph <josekodek2000@gmail.com>
2019-05-03 21:41:35 +0300

    Documenting API

--- Api/Profiles/adminProfile.js
+++ Api/Profiles/adminProfile.js
@@ -10,6 +10,53 @@ const adminProfileController = require('../Controllers/adminUpdateProfile');
 
 adminProfile.get('/:Email/profile', jwtMiddleware, adminProfileController.getAdminProfile);
 
+/**
+* @swagger
+* /admin/signup:
+*   post:
+*     tags:
+*       - Profiles
+*     name: Update Admin Profile
+*     summary: Signs up a customer
+*     consumes:
+*       - multipart/form-data
+*     parameters:
+*       - name: Fullname
+*         in: formData
+*         type: string
+*         required: true
+*         description: Yahya Jalal.
+*       - name: Email
+*         in: formData
+*         type: string
+*         required: true
+*         description: yahya@gmail.com.
+*       - name: Password
+*         in: formData
+*         type: string
+*         required: true
+*         description: stealth.
+*       - name: isAdmin
+*         in: formData
+*         type: string
+*         required: true
+*         description: True or False.
+*       - name: Address
+*         in: formData
+*         type: string
+*         description: Kitende, Entebbe.
+*       - name: uploadImage
+*         in: formData
+*         type: file
+*         required: true
+*         description: Upload an Image File.
+*     responses:
+*       200:
+*         description: User Has Successfully Logged In
+*       401:
+*         description: Invalid Email or Password
+*/
+
 adminProfile.patch('/:Email/profile', jwtMiddleware, settings.upload.single('Image'), adminProfileController.updateAdminProfile);
 
 module.exports = adminProfile;
