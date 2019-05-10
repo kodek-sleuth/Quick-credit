e35c30de62bb0b0d4b7e902a04e9327aa696d40e
Mugerwa Joseph <josekodek2000@gmail.com>
2019-05-10 16:56:40 +0300

    Powering Frontend

--- UI/src/login.form.js
+++ UI/src/login.form.js
@@ -56,7 +56,7 @@ document.getElementsByClassName('loginForm')[0].addEventListener('submit', valid
                 document.getElementById('goodFeedBack').innerHTML = `Welcome back ${data.Data.Fullname}`;
                 
                 document.cookie = `adminToken=${data.Token}; path=/UI/admin_dashboard`;
-
+                document.cookie = `adminEmail=${data.Data.Email}; path=/UI/admin_dashboard;`;
                 window.location = '/Quick-credit/UI/admin_dashboard.html'
             }
         })
@@ -106,9 +106,9 @@ document.getElementsByClassName('loginForm')[0].addEventListener('submit', valid
                 document.getElementById('goodFeedBack').style.display = 'block';
                 document.getElementById('goodFeedBack').innerHTML = `Welcome back ${data.Data.Fullname}`;
                 
-                document.cookie = `userToken=${data.Token}; path=/UI/admin_dashboard`;
-
-                window.location = '/Quick-credit/UI/user_dashboard.html'
+                document.cookie = `userToken=${data.Token}; path=/UI/user_dashboard`;
+                document.cookie = `userEmail=${data.Data.Email}; path=/UI/user_dashboard;`;
+                window.location = '/Quick-credit/UI/user_dashboard.html';
             }
         })
      
---
+++ UI/src/userDashboard.js
@@ -0,0 +1,26 @@
+const getProfile = () => {
+    const cookieToken = document.cookie.split(';')[0].split(' ')[0].split('=')[1];
+    const cookieEmail = document.cookie.split(';')[1].split(' ')[1].split('=')[1];
+
+    console.log(cookieEmail);
+    fetch(`http://localhost:3000/api/v1/user/${cookieEmail}/profile`, {
+        method: 'GET',
+        headers: {
+            Authorization: `Bearer ${cookieToken}`,
+            Accept: 'application/json'
+        }
+    })
+    .then((response) => response.json())
+    .then((data) => {
+        const fullname = data.Data[0].fullname;
+        const email = data.Data[0].email;
+        const image = data.Data[0].image;
+        console.log(image.split('/')[1]);
+
+        document.getElementById('yourName').textContent = fullname;
+        document.getElementById('yourImage').src = 'http://localhost:3000/'+ image.split('/')[1];
+
+    })
+};
+
+getProfile();
