29cb1938a68f304cd16ae0f635d6248ebba07729
Mugerwa Joseph <josekodek2000@gmail.com>
2019-05-13 00:19:50 +0300

    Powering Frontend

--- UI/src/trackUserRepayments.js
+++ UI/src/trackUserRepayments.js
@@ -1,18 +1,10 @@
 const userToken = sessionStorage.getItem('UserToken');
 const userEmail =  sessionStorage.getItem('UserEmail');
 
-const fetchRepayments = () => {
-    fetch(`http://localhost:3000/api/v1/user/${userEmail}/loans/unrepaid`, {
-        method: 'GET',
-        headers: {
-            Authorization: `Bearer ${userToken}`,
-            Accept: 'application/json, text/plain, */*'
-        }
-    })
-    .then((res) => res.json())
-    .then((data) => {
-        
-    })
-};
+document.getElementById('searchRepayments').addEventListener('submit', validateForm = (p) => {
+    p.preventDefault();
 
-fetchRepayments()
+    const loanId = parseInt(document.getElementById('loanId').value);
+
+    
+});
