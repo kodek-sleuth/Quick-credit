3c860b0c3447a4b425919b480a86a579e01a9cae
Mugerwa Joseph <josekodek2000@gmail.com>
2019-05-12 21:00:52 +0300

    Powering Frontend

--- UI/src/userRepayLoan.js
+++ UI/src/userRepayLoan.js
@@ -22,12 +22,40 @@ fetchBalance();
 document.getElementsByClassName('repayLoanForm')[0].addEventListener('submit', validateForm = (p) => {
     p.preventDefault();
 
-    const amount = document.getElementById('amount').value;
+    const currentBalance = document.getElementById('yourBalance').innerHTML;
+
+    const amount = parseFloat(document.getElementById('amount').value);
 
     if (amount == null || amount == '')
     {
         document.getElementById('badFeedBack').style.display = 'block';
         document.getElementById('badFeedBack').innerHTML = 'Please Enter an Amount to repay Balance'
+        return false;
+    }
+
+    if (amount > 20000000)
+    {
+        document.getElementById('badFeedBack').style.display = 'block';
+        document.getElementById('badFeedBack').innerHTML = 'Amount should not be more than 20million'
+        return false;
+    }
+
+    else
+    {
+        const fullname = document.getElementById('yourName').innerHTML;
+        fetch('http://localhost:3000/api/v1/user/loans/repay', {
+            method: 'POST',
+            body: JSON.stringify({Email: userEmail, Fullname: fullname, Amount: amount}),
+            headers: {
+                'Content-Type': 'application/json',  
+                Authorization: `Bearer ${userToken}`,
+                Accept: 'application/json, text/plain, */*'
+            }
+        })
+        .then((res) => res.json())
+        .then((data) => {
+
+        })
     }
 
 
