22dfbba084968e5c09dfe82da865d298f78e9a02
Mugerwa Joseph <josekodek2000@gmail.com>
2019-05-14 12:35:43 +0300

    [#165975472] Creating Admin/User Auth routes

--- api/Controllers/authControllerSignup.js
+++ api/Controllers/authControllerSignup.js
@@ -1,3 +1,5 @@
+/* eslint-disable no-shadow */
+/* eslint-disable indent */
 /* eslint-disable no-restricted-globals */
 /* eslint-disable brace-style */
 /* eslint-disable no-trailing-spaces */
@@ -58,7 +60,104 @@ exports.signUpUser = (req, res, next) => {
         isAdmin: req.body.isAdmin
       };   
 
-      admins.push(newUser);
+      admins.push(newUser); if (req.body.Email == null || req.body.Email == '' && req.body.Password == null || req.body.Password == '' && req.body.Fullname == null || req.body.Fullname == '') {
+        res.status(400).json({
+          Status: 400,
+          Error: 'Email, Fullname and Password fields are required'
+        });
+      } 
+  
+      if (isNaN(req.body.Email) == false || isNaN(req.body.Fullname) == false)
+      {
+        res.status(400).json({
+          Status: 400,
+          Error: 'Email and Fullname cannot be Integer'
+        });
+      }
+  
+      else {
+        admins.forEach((admin) => {
+          if (admin.Email == req.body.Email || admin.Fullname == req.body.Fullname) {
+            res.status(400).json({
+              Status: 400,
+              Error: 'Email or Name is already taken'
+            });
+          }
+        });
+          
+        const newUser = {
+          Email: req.body.Email,
+          Fullname: req.body.Fullname,
+          Password: req.body.Password,
+          isAdmin: req.body.isAdmin
+        };   
+  
+        admins.push(newUser);
+  
+        res.status(201).json({
+          Status: 201,
+          Data: {
+            Email: req.body.Email,
+            Fullname: req.body.Fullname,
+            Password: req.body.Password,
+            isAdmin: req.body.isAdmin
+          },
+          Admins: admins,
+          Success: 'Admin successfully signed up' 
+        });
+      }
+    } 
+
+    res.status(201).json({
+      Status: 201,
+      Data: {
+        Email: req.body.Email,
+        Fullname: req.body.Fullname,
+        Password: req.body.Password,
+        isAdmin: req.body.isAdmin
+      },
+      Admins: admins,
+      Success: 'Admin successfully signed up' 
+    });
+  }
+
+  if (req.body.isAdmin == 'False')
+  {
+    if (req.body.Email == null || req.body.Email == '' && req.body.Password == null || req.body.Password == '' && req.body.Fullname == null || req.body.Fullname == '' && req.body.Address == '' || req.body.Address == null) {
+      res.status(400).json({
+        Status: 400,
+        Error: 'Email, Fullname, Password and Address fields are required'
+      });
+    } 
+
+    if (isNaN(req.body.Email) == false || isNaN(req.body.Fullname) == false)
+    {
+      res.status(400).json({
+        Status: 400,
+        Error: 'Email and Fullname cannot be Integer'
+      });
+    }
+
+    else {
+      users.forEach((admin) => {
+        if (admin.Email == req.body.Email || admin.Fullname == req.body.Fullname) {
+          res.status(400).json({
+            Status: 400,
+            Error: 'Email or Name is already taken'
+          });
+        }
+      });
+          
+      const newUser2 = {
+        Email: req.body.Email,
+        Fullname: req.body.Fullname,
+        Password: req.body.Password,
+        isAdmin: req.body.isAdmin,
+        Address: req.body.Address,
+        Status: 'Pending'
+      };   
+
+      users.push(newUser2);
 
       res.status(201).json({
         Status: 201,
@@ -66,11 +165,21 @@ exports.signUpUser = (req, res, next) => {
           Email: req.body.Email,
           Fullname: req.body.Fullname,
           Password: req.body.Password,
-          isAdmin: req.body.isAdmin
+          isAdmin: req.body.isAdmin,
+          Status: 'Pending',
+          Address: req.body.Address
         },
-        Admins: admins,
-        Success: 'Admin successfully signed up' 
+        Users: users,
+        Success: 'User successfully signed up' 
       });
     }
+
+      if (req.body.isAdmin == '' || req.body.isAdmin == null)
+      {
+        res.status(400).json({
+          Status: 400,
+          Error: 'isAdmin is a required field'
+        });
+      }
   }
 };
--- api/Controllers/authLoginController.js
+++ api/Controllers/authLoginController.js
@@ -66,4 +66,57 @@ exports.loginUser = (req, res, next) => {
       });
     }
   }
+
+  if (req.body.isAdmin == 'False')
+  {
+    if (req.body.Email == null || req.body.Email == '' && req.body.Password == null || req.body.Password == '' && req.body.Address == '' || req.body.Address == null) {
+      res.status(400).json({
+        Status: 400,
+        Error: 'Email, Fullname, Password, Address fields are required'
+      });
+    } 
+
+    if (isNaN(req.body.Email) == false)
+    {
+      res.status(400).json({
+        Status: 400,
+        Error: 'Email cannot be Integer'
+      });
+    }
+
+    else {
+      users.forEach((user) => {
+        if (user.Email == req.body.Email && user.Password == req.body.Password) {
+          res.status(200).json({
+            Status: 200,
+            Data: {
+              Email: user.Email,
+              Fullname: user.Fullname,
+              Password: user.Password,
+              isAdmin: user.isAdmin,
+              Address: user.Address,
+              Status: user.Status
+            },
+            Success: 'User successfully logged up' 
+          });
+        }
+
+        else
+        {
+          res.status(401).json({
+            Status: 401,
+            Error: 'Invalid Email or Password'
+          });
+        }
+      });
+    }
+  }
+
+  if (req.body.isAdmin == null || req.body.isAdmin == null)
+  {
+    res.status(400).json({
+      Status: 400,
+      Error: 'isAdmin is a required field'
+    });
+  }
 };
