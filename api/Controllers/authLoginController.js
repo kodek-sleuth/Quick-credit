9f463b6ac823a6ce3774d98db8743d6ce263a05b
Mugerwa Joseph <josekodek2000@gmail.com>
2019-05-14 21:31:21 +0300

    Fixing APIs

--- api/Controllers/authLoginController.js
+++ api/Controllers/authLoginController.js
@@ -1,51 +1,41 @@
 /* eslint-disable no-restricted-globals */
-/* eslint-disable brace-style */
-/* eslint-disable no-trailing-spaces */
-/* eslint-disable padded-blocks */
 /* eslint-disable no-mixed-operators */
 /* eslint-disable eqeqeq */
-/* eslint-disable no-empty */
 /* eslint-disable comma-dangle */
 
 const models = require('../Models/models');
 
 exports.loginUser = (req, res, next) => {
-  if (req.body.isAdmin == 'False')
-  {
+  if (req.body.isAdmin == 'False') {
     if (req.body.Email == null || req.body.Email == '' && req.body.Password == null || req.body.Password == '') {
       res.status(400).json({
         Status: 400,
         Error: 'Email and Password fields are required'
       });
-    } 
+    }
 
-    if (isNaN(req.body.Email) == false)
-    {
+    if (isNaN(req.body.Email) == false) {
       res.status(400).json({
         Status: 400,
         Error: 'Email cannot be Integer'
       });
-    }
-
-    else {
+    } else {
       models.users.forEach((user) => {
         if (user.Email == req.body.Email && user.Password == req.body.Password) {
           res.status(200).json({
             Status: 200,
             Data: {
               Email: user.Email,
-              Fullname: user.Fullname,
+              Firstname: user.Fullname,
+              Lastname: user.Lastname,
               Password: user.Password,
               isAdmin: user.isAdmin,
               Address: user.Address,
               Status: user.Status
             },
-            Success: 'User successfully logged up' 
+            Success: 'User successfully logged up'
           });
-        }
-
-        else
-        {
+        } else {
           res.status(401).json({
             Status: 401,
             Error: 'Invalid Email or Password'
